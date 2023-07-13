bfbaa8209be4422478507e54ae2040d4f9f60655
EkoHajiantoSula <ekohajiantosula@gmail.com>
2023-07-14 02:46:59 +0700

    first commit

---
+++ database/migrations/2021_04_14_081552_create_parkir_masuk_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParkirMasukTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parkir_masuk', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->string('plat_nomor');
+            $table->string('jenis_kendaraan');
+            $table->string('jam_masuk');
+            $table->string('tgl_masuk');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parkir_masuk');
+    }
+}
